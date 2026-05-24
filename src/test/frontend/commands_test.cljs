37e0e2bdc23a28f8bdd1ded261e99fbb6e7cedf6
Planning Agent <planning-agent@pa-to-po.dev>
2026-05-24 01:51:00 +0000

    Add kura slash command template

---
+++ src/test/frontend/commands_test.cljs
@@ -0,0 +1,12 @@
+(ns frontend.commands-test
+  (:require [cljs.test :refer [deftest is testing]]
+            [frontend.commands :as commands]))
+
+(deftest kura-task-slash-command-test
+  (testing "/kura inserts the kura task template"
+    (let [command (->> (commands/commands-map identity)
+                       (filter #(= "kura" (first %)))
+                       first)]
+      (is (= "kura" (first command)))
+      (is (= [[:editor/input "LATER #kuratask\n\t-" {:last-pattern "/"}]]
+             (second command))))))
